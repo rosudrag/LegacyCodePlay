9887c3de2c7067527b5ffc523ae756431a67268a
Dragos Rosu <dragos.romario@codecomputerlove.com>
2015-08-13 11:29:56 +0100

    first stage

---
+++ Trivia/GameRunner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UglyTrivia;
+
+namespace Trivia
+{
+    public class GameRunner
+    {
+
+        private static bool notAWinner;
+
+        public static void Main(String[] args)
+        {
+            RunGame(new Random());
+        }
+
+        public static void RunGame(Random random)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            do
+            {
+
+                aGame.roll(random.Next(5) + 1);
+
+                if (random.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+        }
+
+
+    }
+
+}
+
---
+++ Trivia/GameTests.cs
@@ -0,0 +1,114 @@
+﻿namespace Trivia
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using NUnit.Framework;
+    using NUnit.Framework.Constraints;
+    using UglyTrivia;
+
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void MyGameHasOnePlayerAfterAddingOnePlayer()
+        {
+            var myGame = new Game();
+
+            myGame.add("myPlayer");
+
+            Assert.AreEqual(myGame.howManyPlayers(), 1);
+        }
+
+        [Test]
+        public void GameIsNotPlayableWithoutPlayers()
+        {
+            var myGame = new Game();
+
+            var playable = myGame.isPlayable();
+
+            Assert.False(playable);
+        }
+
+        [Test]
+        public void GameIsNotPlayableWithOnePlayer()
+        {
+            var myGame = new Game();
+
+            myGame.add("player one");
+
+            Assert.False(myGame.isPlayable());
+        }
+
+        [Test]
+        public void GameIsPlayableWithTwoPlayers()
+        {
+            var myGame = new Game();
+
+            myGame.add("player one");
+            myGame.add("player two");
+
+            Assert.True(myGame.isPlayable());
+        }
+
+        [Test]
+        public void GameIsNotPlayableWithMoreThan5Players()
+        {
+            var myGame = new Game();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                myGame.add("player " + i);
+            }
+
+            Assert.Throws<IndexOutOfRangeException>(() => { myGame.add("I crash"); });
+
+        }
+
+        [Test]
+        public void NewGameHasZeroPlayers()
+        {
+            var myGame = new Game();
+
+            Assert.AreEqual(myGame.howManyPlayers(), 0);
+        }
+
+        [Test]
+        [Ignore]
+        public void GenerateGoldenMasterOutput()
+        {
+            var builder = new StringBuilder();
+            var logger = new StringWriter(builder);
+            Console.SetOut(logger);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                GameRunner.RunGame(new Random(i));
+            }
+
+            var output = builder.ToString();
+
+            File.WriteAllText(Path.GetTempPath() + "golden_master.txt", output);
+        }
+
+        [Test]
+        public void TestGameOutputAgainstGoldenMaster()
+        {
+            var expectedOutput = File.ReadAllText(Path.GetTempPath() + "golden_master.txt");
+
+            var builder = new StringBuilder();
+            var logger = new StringWriter(builder);
+            Console.SetOut(logger);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                GameRunner.RunGame(new Random(i));
+            }
+
+            var output = builder.ToString();
+
+            Assert.AreEqual(expectedOutput, output, "Game output does not match the golden master");
+        }
+
+    }
+}
